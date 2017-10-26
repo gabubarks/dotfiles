6d3708d6d4633e42619e02101e81096a3f338254
Gabu <locallupine@gmail.com>
2017-10-26 15:11:23 +0200

    Update

--- spacemacs.d/init.el
+++ spacemacs.d/init.el
@@ -49,7 +49,8 @@ values."
 
      ;; === BEHAVIOUR ===
      helm
-     auto-completion
+     (auto-completion :variables
+                      auto-completion-enable-snippets-in-popup t)
      emacs-lisp
      git
      (version-control :variables
@@ -59,7 +60,14 @@ values."
      ;; === LANGUAGES ===
      python
      (c-c++ :variables
-            c-c++-default-mode-for-headers 'c++-mode)
+            c-c++-default-mode-for-headers 'c++-mode
+            c-c++-enable-clang-support t)
+     javascript
+     windows-scripts
+     yaml
+     markdown
+     nginx
+     docker
 
      ;; === APPEARANCE ===
      themes-megapack
@@ -72,7 +80,8 @@ values."
    ;; A list of packages that cannot be updated.
    dotspacemacs-frozen-packages '()
    ;; A list of packages that will not be installed and loaded.
-   dotspacemacs-excluded-packages '(firebelly-theme
+   dotspacemacs-excluded-packages '(fill-column-indicator   ;; breaks magit commit for emacs version < 24
+                                    firebelly-theme
                                     niflheim-theme
                                     pastels-on-dark-theme
                                     tronesque-theme)
@@ -374,45 +383,48 @@ you should place your code here."
  '(compilation-message-face (quote default))
  '(evil-want-Y-yank-to-eol nil)
  '(fci-rule-color "#424748" t)
+ '(helm-grep-ignored-directories
+   (quote
+    ("SCCS/" "RCS/" "CVS/" "MCVS/" ".svn/" ".git/" ".hg/" ".bzr/" "_MTN/" "_darcs/" "{arch}/" ".gvfs/")))
  '(highlight-changes-colors (quote ("#ff8eff" "#ab7eff")))
  '(highlight-tail-colors
-        (quote
-         (("#424748" . 0)
-          ("#63de5d" . 20)
-          ("#4BBEAE" . 30)
-          ("#1DB4D0" . 50)
-          ("#9A8F21" . 60)
-          ("#A75B00" . 70)
-          ("#F309DF" . 85)
-          ("#424748" . 100))))
+   (quote
+    (("#424748" . 0)
+     ("#63de5d" . 20)
+     ("#4BBEAE" . 30)
+     ("#1DB4D0" . 50)
+     ("#9A8F21" . 60)
+     ("#A75B00" . 70)
+     ("#F309DF" . 85)
+     ("#424748" . 100))))
  '(magit-diff-use-overlays nil)
- '(neo-mode-line-type (quote neotree))
+ '(neo-mode-line-type (quote none))
  '(pos-tip-background-color "#E6DB74")
  '(pos-tip-foreground-color "#242728")
  '(vc-annotate-background nil)
  '(vc-annotate-color-map
-        (quote
-         ((20 . "#ff0066")
-          (40 . "#CF4F1F")
-          (60 . "#C26C0F")
-          (80 . "#E6DB74")
-          (100 . "#AB8C00")
-          (120 . "#A18F00")
-          (140 . "#989200")
-          (160 . "#8E9500")
-          (180 . "#63de5d")
-          (200 . "#729A1E")
-          (220 . "#609C3C")
-          (240 . "#4E9D5B")
-          (260 . "#3C9F79")
-          (280 . "#53f2dc")
-          (300 . "#299BA6")
-          (320 . "#2896B5")
-          (340 . "#2790C3")
-          (360 . "#06d8ff"))))
+   (quote
+    ((20 . "#ff0066")
+     (40 . "#CF4F1F")
+     (60 . "#C26C0F")
+     (80 . "#E6DB74")
+     (100 . "#AB8C00")
+     (120 . "#A18F00")
+     (140 . "#989200")
+     (160 . "#8E9500")
+     (180 . "#63de5d")
+     (200 . "#729A1E")
+     (220 . "#609C3C")
+     (240 . "#4E9D5B")
+     (260 . "#3C9F79")
+     (280 . "#53f2dc")
+     (300 . "#299BA6")
+     (320 . "#2896B5")
+     (340 . "#2790C3")
+     (360 . "#06d8ff"))))
  '(vc-annotate-very-old-color nil)
  '(weechat-color-list
-        (unspecified "#242728" "#424748" "#F70057" "#ff0066" "#86C30D" "#63de5d" "#BEB244" "#E6DB74" "#40CAE4" "#06d8ff" "#FF61FF" "#ff8eff" "#00b2ac" "#53f2dc" "#f8fbfc" "#ffffff")))
+   (unspecified "#242728" "#424748" "#F70057" "#ff0066" "#86C30D" "#63de5d" "#BEB244" "#E6DB74" "#40CAE4" "#06d8ff" "#FF61FF" "#ff8eff" "#00b2ac" "#53f2dc" "#f8fbfc" "#ffffff")))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
