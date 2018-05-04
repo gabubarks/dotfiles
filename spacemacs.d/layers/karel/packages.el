f48d01fe448daf34e696fab6cfdf7704c0c3a615
BlizzardFenrir <jan.veldthuis@gmail.com>
2018-05-04 16:17:49 +0200

    Update

--- spacemacs.d/layers/karel/packages.el
+++ spacemacs.d/layers/karel/packages.el
@@ -2,7 +2,7 @@
 ;;
 ;; Copyright (c) 2012-2017 Sylvain Benner & Contributors
 ;;
-;; Author: Jan <jan@9S>
+;; Author: Gabu <locallupine@gmail.com>
 ;; URL: https://github.com/syl20bnr/spacemacs
 ;;
 ;; This file is not part of GNU Emacs.
