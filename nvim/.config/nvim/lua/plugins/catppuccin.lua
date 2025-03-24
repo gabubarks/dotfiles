219903f850d68f53fc6fd45448c0b12a476bdd94
Gabu <wildgabu@gmail.com>
2025-03-24 11:45:56 +0100

    Use undercurl with Catppuccin

--- nvim/.config/nvim/lua/plugins/catppuccin.lua
+++ nvim/.config/nvim/lua/plugins/catppuccin.lua
@@ -2,8 +2,28 @@
 return {
   {
     "catppuccin",
+    lazy = true,
     opts = function(_, opts)
+      -- Set flavor
       opts.flavour = "macchiato"
+
+      -- Use undercurl, defaults to underline otherwise
+      opts.integrations = vim.tbl_deep_extend(
+        "force",
+        opts.integrations,
+        {
+          native_lsp = {
+            underlines = {
+              errors = { "undercurl" },
+              hints = { "undercurl" },
+              warnings = { "undercurl" },
+              information = { "undercurl" },
+              ok = { "undercurl" },
+            },
+          },
+        }
+      )
+
       return opts
     end,
   },
